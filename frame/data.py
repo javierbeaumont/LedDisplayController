45ae3d61fbf3ae771953036eb112918906de509a
Javier Beaumont <javier.beaumont@socialpoint.es>
2013-05-31 14:45:23 +0200

    Fix model. Support +/- 4 screens

--- frame/data.py
+++ frame/data.py
@@ -8,8 +8,8 @@ class DataFrame:
     SIGN = [0x7f, 0x6f, 0x7e, 0x6e, 0x7d, 0x6d]
 
     def __init__(self, settings, display_number = 0):
-        self.width =  settings['screen']['width']
-        self.height =  settings['screen']['height']
+        self.width = round(settings['screen']['width'] / 8)
+        self.height = settings['screen']['height']
         self.frames_bytes = settings['frames']['bytes']
 
         # Define Display Number
@@ -17,53 +17,36 @@ class DataFrame:
 
         # Define Frame Layout
         self.frames_total_bytes = sum(self.frames_bytes.values())
-        self.bytes = [0] * self.frames_total_bytes
 
     def get(self, data = []):
-        data_bytes = []
-        for h in range(len(data)):
-            i = 0
-            data_bytes_rows = []
-            while len(data[h]):
-                data_width = min(self.width, len(data[h]))
-                data_bytes_rows.append(
-                    data[h][:data_width] + [0] * (self.width - data_width)
-                )
-                data[h] = data[h][data_width:]
-                i += 1
-
-            data_bytes.append(data_bytes_rows)
-
-        count = 0
-        data_format = []
-        for col in zip(*data_bytes):
-            data_format_frame = []
-            for row in col:
-                data_format_frame += row
-                count += len(row)
-
-            data_format.append(data_format_frame)
-
-
-        bytes = self.bytes
-
         frame_data = []
-        frames = max(math.ceil(count / (self.frames_bytes['data'])), 2)
+        frames = max(2, 2)
         for f in range(frames):
+            bytes = [0] * self.frames_total_bytes
             if f == 0:
                 sign = self.SIGN[0]
+                data_bytes = data
             elif f + 1 == frames:
-                sign = self.SIGN[5]
+                sign = self.SIGN[4]
+                data_bytes = [0]
             else:
-                sign = self.SIGN[3]
+                sign = self.SIGN[2]
+                data_bytes = [0]
 
             bytes[0] = self.display_number
             bytes[1] = int(sign)
-            bytes[2:len(data_format[f])] = data_format[f]
+
+            bytes[11:len(data_bytes) + 11] = data_bytes
 
             checksum = self.frames_bytes['checksum']
             bytes[self.frames_total_bytes - checksum] = sum(bytes) % 256
 
             frame_data += bytes
 
-        return frame_data
+        return {
+            'data': frame_data,
+            'screen': {
+                'amount': frames,
+                'last': frames - 1
+            }
+        }
